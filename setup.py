437846d4611da02550a80ecd1c81f544b443cd8a
JacoBSmitH101 <47941474+JacoBSmitH101@users.noreply.github.com>
2024-05-10 10:50:55 +0100

    I love natan

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='NatanStanek',
-    version='0.1',
-    description='A simple demonstration package for NatanStanek',
+    version='0.14.2',
+    description='The real Natan Stanek python package',
     url='https://github.com/JacoBSmitH101/NatanStanek',
     author='James Mortimer',
     author_email='jamesmortimer@bournside.smells.uk',
